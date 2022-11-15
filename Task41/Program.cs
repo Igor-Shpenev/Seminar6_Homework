d028cfc0680fc0a81f7c39dda0e08c007651b7c6
Igor Shpenev <vdvshneek@yandex.ru>
2022-11-15 16:41:25 +0500

    Completed task number 41

---
+++ Task41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int PlayGame(int num)
+{
+    int count = 0;
+    int num2 = 0; 
+    for (int i = 1; i <= num; i++)
+    {
+        
+        Console.Write("Input number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        if (num2 > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("My friend, how many numbers do you want to enter?: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("The answer is accepted. The game begins!");
+Console.WriteLine($"The number of positive numbers is {PlayGame(num)}");
